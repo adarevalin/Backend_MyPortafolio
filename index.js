d855f98be3658c2bf97705e1d5eec3c147bad236
Andres David <kyfunky1386@gmail.com>
2024-02-15 16:34:26 -0500

    url

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 
 app.use(cors({ 
   credentials: true, 
-  origin: ['http://localhost:3000', 'https://frontend-myportafolio.onrender.com', 'https://3001-cs-b69fbd4a-00fd-41b8-a255-b60bdb28f0d1.cs-us-east1-pkhd.cloudshell.dev'] 
+  origin: ['http://localhost:3000', 'https://portafolio-nr11.onrender.com'] 
 }));
 
 app.use(cookieParser()); // Configura cookie-parser
